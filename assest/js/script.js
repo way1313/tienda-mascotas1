ae30c20067433914ba3e148ffe7d95414de4146e
Wilfer Yepes <wilferyepes@hotmail.com>
2026-03-18 21:04:20 -0500

    responsive

--- assest/js/script.js
+++ assest/js/script.js
@@ -15,3 +15,28 @@ const video   = document.getElementById('video');
       playBtn.classList.remove('hide');
     }
   });
+
+  const btn = document.querySelector('.hamburger');
+  const nav = document.querySelector('.nav-menu');
+
+  // Toggle menú
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('is-active');
+    nav.classList.toggle('is-open');
+  });
+
+  // Cerrar menú al hacer scroll
+  window.addEventListener('scroll', () => {
+    if (nav.classList.contains('is-open')) {
+      btn.classList.remove('is-active');
+      nav.classList.remove('is-open');
+    }
+  });
+
+  // Cerrar menú al clickear un enlace
+  document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+      btn.classList.remove('is-active');
+      nav.classList.remove('is-open');
+    });
+  });
